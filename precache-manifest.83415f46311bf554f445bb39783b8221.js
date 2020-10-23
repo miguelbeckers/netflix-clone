8de7ca204ea45934a15d332ce3c47e7860d96d5c
miguelbeckers <52552717+miguelbeckers@users.noreply.github.com>
2020-10-23 18:06:08 -0300

    Updates

---
+++ precache-manifest.83415f46311bf554f445bb39783b8221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c823752e6e421c0009a038d71bd3899",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "7a879c3eed9c603ff470",
+    "url": "/netflix-clone/static/css/main.54f1e9ff.chunk.css"
+  },
+  {
+    "revision": "535a86135a77235fafd8",
+    "url": "/netflix-clone/static/js/2.4180f930.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/netflix-clone/static/js/2.4180f930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a879c3eed9c603ff470",
+    "url": "/netflix-clone/static/js/main.9f878012.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
